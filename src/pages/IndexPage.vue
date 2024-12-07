3a379686b453c827f619962e08f4513e852ef00c
1G000 <gorbatovskiy.igor@gmail.com>
2024-12-07 16:50:33 +0300

    feat: add advantages section

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,11 @@
 <template>
   <the-slider></the-slider>
+  <TheAdvantages />
 </template>
 
 <script setup>
 import TheSlider from "src/components/TheSlider.vue";
+import TheAdvantages from "src/components/TheAdvantages.vue";
 defineOptions({
   name: "IndexPage",
 });
