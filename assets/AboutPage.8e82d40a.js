a5f17370bbba561102af48bc0796fc6a11caa278
tatyana <tilkka@mail.ru>
2024-12-22 22:55:37 +0300

    Updates

--- assets/AboutPage.8e82d40a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{F as r,I as o}from"./index.e5d56380.js";const t={};function c(a,n){return r(),o("h1",null,"ti pidor")}var f=e(t,[["render",c]]);export{f as default};
