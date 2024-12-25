1e739885c87a9d42af778cb1cb022e118810962c
tatyana <tilkka@mail.ru>
2024-12-26 01:18:13 +0300

    Updates

---
+++ assets/UiSectionTitle.c1111a6b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.1bcdb6ec.js";import{F as _,I as i,M as o}from"./index.4919be06.js";const s={class:"section__title"},c={__name:"UiSectionTitle",props:{titleText:{type:String}},setup(t){return(r,a)=>(_(),i("h2",s,o(t.titleText),1))}};var p=e(c,[["__scopeId","data-v-fb402156"]]);export{p as U};
