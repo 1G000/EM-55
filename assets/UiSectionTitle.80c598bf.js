c22d6657a22c0a55600ae73be9ce316678cdc1c7
User <gorbatovskiy.igor@gmail.com>
2024-12-27 00:42:57 +0300

    Updates

--- assets/UiSectionTitle.80c598bf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.e9d61943.js";import{F as _,I as i,M as o}from"./index.c3a5ac63.js";const s={class:"section__title"},c={__name:"UiSectionTitle",props:{titleText:{type:String}},setup(t){return(r,a)=>(_(),i("h2",s,o(t.titleText),1))}};var p=e(c,[["__scopeId","data-v-fb402156"]]);export{p as U};
