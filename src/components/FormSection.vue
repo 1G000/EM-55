f5a2f017ee53918c5c69b40bad79dfd1782221ca
User <gorbatovskiy.igor@gmail.com>
2025-01-20 11:21:25 +0300

    fix: form bg

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -14,8 +14,8 @@ const tel = ref(null);
 const files = ref(null);
 const accept = ref(false);
 // const privacyDialog = ref(false);
-const sectionBackground = `url('./Images/Form/form-background.jpg')`;
-const sectionBackgroundMobile = `url('./Images/Form/form-background-mobile.jpg')`;
+const sectionBackground = `url('../Images/Form/form-background.jpg')`;
+const sectionBackgroundMobile = `url('../Images/Form/form-background-mobile.jpg')`;
 const submitForm = () => {
   // $q.notify({
   //   color: 'green-4',
