2780bfd2cf8b7bdfbbbe9a56f64360680d696aad
Nikonova Tatiana <t.nikonova@rivc-pulkovo.ru>
2025-01-24 15:53:31 +0300

    Updates

--- assets/use-timeout.2da795fa.js
+++
@@ -1,1 +0,0 @@
-import{ah as i,a as r,ai as a,g as s}from"./index.a2619799.js";function l(){let e=null;const n=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),r(t),{removeTimeout:t,registerTimeout(o,u){t(),a(n)===!1&&(e=setTimeout(()=>{e=null,o()},u))}}}export{l as u};
