40ce6ed97dca74610cb1287ce39b358792cac1b0
tatyana <tilkka@mail.ru>
2025-01-25 23:17:08 +0300

    Updates

---
+++ assets/use-timeout.c3f4c491.js
@@ -0,0 +1,1 @@
+import{ag as r,a,ah as i,g as s}from"./index.4f5c2120.js";function l(){let e=null;const n=s();function t(){e!==null&&(clearTimeout(e),e=null)}return r(t),a(t),{removeTimeout:t,registerTimeout(o,u){t(),i(n)===!1&&(e=setTimeout(()=>{e=null,o()},u))}}}export{l as u};
