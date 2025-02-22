b902c9b5b64105300de302b0e5a45b734bdce783
tatyana <tilkka@mail.ru>
2025-02-22 23:33:02 +0300

    feat: install yandex map

--- src/pages/SmrPage.vue
+++ src/pages/SmrPage.vue
@@ -1,8 +1,31 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import UiSection from "src/components/UiSection.vue";
 import UiSectionTitle from "src/components/UiSectionTitle.vue";
 import BreadCrumbs from "src/components/BreadCrumbs.vue";
+import { shallowRef } from "vue";
+
+import {
+  YandexMap,
+  YandexMapDefaultSchemeLayer,
+  YandexMapDefaultFeaturesLayer,
+  YandexMapDefaultMarker,
+  YandexMapMarker,
+} from "vue-yandex-maps";
+
 const sectionTitle = "Строительно-монтажные работы";
+const map = shallowRef(null);
+
+const markers = [
+  {
+    coordinates: [30.28961, 59.942786],
+    title: "Что-то 1",
+  },
+  {
+    coordinates: [30.3351, 59.9343],
+    title: "Что-то 2",
+  },
+];
 </script>
 
 <template>
@@ -12,7 +35,28 @@ const sectionTitle = "Строительно-монтажные работы";
   >
     <BreadCrumbs :page-route="sectionTitle" />
     <UiSectionTitle tag="h1" :title-text="sectionTitle" />
-
+    <yandex-map
+      v-model="map"
+      :settings="{
+        location: {
+          center: [30.3351, 59.9343],
+          zoom: 12,
+        },
+      }"
+      width="100%"
+      height="500px"
+    >
+      <yandex-map-default-scheme-layer />
+      <yandex-map-default-features-layer />
+      <yandex-map-default-marker
+        v-for="marker in markers"
+        :key="marker.title"
+        :settings="marker"
+      />
+      <!-- <yandex-map-marker v-for="marker in markers" :key="marker.title" :settings="marker"> -->
+      <!-- <div class="marker"></div> -->
+      <!-- </yandex-map-marker> -->
+    </yandex-map>
     <q-img src="Images/Gallery/1.png"> </q-img>
 
     <q-img src="Images/Gallery/2.png"> </q-img>
@@ -20,3 +64,19 @@ const sectionTitle = "Строительно-монтажные работы";
     <q-img src="Images/Gallery/3.png"> </q-img>
   </UiSection>
 </template>
+
+<style scoped>
+.marker {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  background: #ff0000;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  line-height: 20px;
+}
+</style>
