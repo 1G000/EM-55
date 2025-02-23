b181010902d0efa95fbe045335ab0b62d05f1b92
tatyana <tilkka@mail.ru>
2025-02-23 18:06:58 +0300

    feat: add markers and hints

--- src/pages/SmrPage.vue
+++ src/pages/SmrPage.vue
@@ -4,6 +4,7 @@ import UiSection from "src/components/UiSection.vue";
 import UiSectionTitle from "src/components/UiSectionTitle.vue";
 import BreadCrumbs from "src/components/BreadCrumbs.vue";
 import { shallowRef } from "vue";
+import ComplexServiceSection from "src/components/ComplexServiceSection.vue";
 
 import {
   YandexMap,
@@ -11,6 +12,9 @@ import {
   YandexMapDefaultFeaturesLayer,
   YandexMapDefaultMarker,
   YandexMapMarker,
+  YandexMapHint,
+  YandexMapControls,
+  YandexMapZoomControl,
 } from "vue-yandex-maps";
 
 const sectionTitle = "Строительно-монтажные работы";
@@ -18,12 +22,28 @@ const map = shallowRef(null);
 
 const markers = [
   {
-    coordinates: [30.28961, 59.942786],
-    title: "Что-то 1",
+    coordinates: [30.296384, 59.913753],
+    properties: {
+      hint: "<b>Рижский пр. д. 4-6 - Монтаж БКТП</b>",
+    },
   },
   {
-    coordinates: [30.3351, 59.9343],
-    title: "Что-то 2",
+    coordinates: [30.283592, 59.927924],
+    properties: {
+      hint: "<b>Набережная реки Мойки, 122 - Монтаж БКТП</b>",
+    },
+  },
+  {
+    coordinates: [30.335137, 59.92272],
+    properties: {
+      hint: "<b>Звенигородская улица, 1 - Монтаж БКТП</b>",
+    },
+  },
+  {
+    coordinates: [30.356086, 59.937215],
+    properties: {
+      hint: "<b>Ковенский переулок, 5 - Монтаж БКТП</b>",
+    },
   },
 ];
 </script>
@@ -41,6 +61,7 @@ const markers = [
         location: {
           center: [30.3351, 59.9343],
           zoom: 12,
+          showScaleInCopyrights: true,
         },
       }"
       width="100%"
@@ -48,14 +69,17 @@ const markers = [
     >
       <yandex-map-default-scheme-layer />
       <yandex-map-default-features-layer />
-      <yandex-map-default-marker
-        v-for="marker in markers"
-        :key="marker.title"
-        :settings="marker"
-      />
-      <!-- <yandex-map-marker v-for="marker in markers" :key="marker.title" :settings="marker"> -->
-      <!-- <div class="marker"></div> -->
-      <!-- </yandex-map-marker> -->
+      <yandex-map-controls :settings="{ position: 'right' }">
+        <yandex-map-zoom-control />
+      </yandex-map-controls>
+      <yandex-map-marker v-for="marker in markers" :key="marker.title" :settings="marker">
+        <div class="marker"></div>
+      </yandex-map-marker>
+      <yandex-map-hint hint-property="hint">
+        <template #default="{ content }">
+          <div class="hint-window" v-html="content" />
+        </template>
+      </yandex-map-hint>
     </yandex-map>
     <q-img src="Images/Gallery/1.png"> </q-img>
 
@@ -70,7 +94,7 @@ const markers = [
   position: relative;
   width: 20px;
   height: 20px;
-  background: #ff0000;
+  background: var(--q-primary);
   border-radius: 50%;
   border: 2px solid #fff;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
@@ -78,5 +102,15 @@ const markers = [
   color: #fff;
   font-weight: bold;
   line-height: 20px;
+}
+.hint-window {
+  position: absolute;
+  transform: translate(7px, -100%);
+  padding: 4px 20px;
+  background: var(--q-primary);
+  color: var(--color-white);
+  border: 1px solid var(--q-primary);
+  white-space: nowrap;
+  border-radius: 4px;
 }
 </style>
