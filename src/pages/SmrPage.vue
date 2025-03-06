ed76ea2f5bda17216fbcd13ff5e614d1ad8bfe3a
tatyana <tilkka@mail.ru>
2025-03-06 08:07:20 +0300

    feat: popup on map

--- src/pages/SmrPage.vue
+++ src/pages/SmrPage.vue
@@ -24,28 +24,38 @@ const markers = [
   {
     coordinates: [30.296384, 59.913753],
     properties: {
-      hint: "<div style='display: flex; flex-direction: column; gap: 5px'><b>Рижский пр. д. 4-6</b><b>Монтаж БКТП</b></div>",
+      src: "Images/Gallery/1.png",
+      address: "Рижский пр. д. 4-6",
+      title: "Монтаж БКТП",
     },
   },
   {
     coordinates: [30.283592, 59.927924],
     properties: {
-      hint: "<div style='display: flex; flex-direction: column; gap: 5px'><b>Наб. реки Мойки, 122</b><b>Монтаж БКТП</b></div>",
+      src: "Images/Gallery/1.png",
+      address: "Звенигородская ул., 1",
+      title: "Монтаж БКТП",
     },
   },
   {
     coordinates: [30.335137, 59.92272],
     properties: {
-      hint: "<div style='display: flex; flex-direction: column; gap: 5px'><b>Звенигородская ул., 1</b><b>Монтаж БКТП</b></div>",
+      src: "Images/Gallery/3.png",
+      address: "Звенигородская ул., 1",
+      title: "Монтаж БКТП",
     },
   },
   {
     coordinates: [30.356086, 59.937215],
     properties: {
-      hint: "<div style='display: flex; flex-direction: column; gap: 5px'><b>Ковенский пер., 5</b><b>Монтаж БКТП</b></div>",
+      src: "Images/Gallery/2.png",
+      address: "Звенигородская ул., 1",
+      title: "Монтаж БКТП",
     },
   },
 ];
+
+const openMarker = ref(null);
 </script>
 
 <template>
@@ -57,6 +67,7 @@ const markers = [
     <UiSectionTitle tag="h1" :title-text="sectionTitle" />
     <yandex-map
       v-model="map"
+      @click.stop="openMarker = null"
       :settings="{
         location: {
           center: [30.3351, 59.9343],
@@ -72,7 +83,7 @@ const markers = [
       <yandex-map-controls :settings="{ position: 'right' }">
         <yandex-map-zoom-control />
       </yandex-map-controls>
-      <yandex-map-marker
+      <!-- <yandex-map-marker
         v-for="marker in markers"
         :key="marker.title"
         :settings="marker"
@@ -83,7 +94,31 @@ const markers = [
         <template #default="{ content }">
           <div class="hint-window" v-html="content" />
         </template>
-      </yandex-map-hint>
+      </yandex-map-hint> -->
+      <yandex-map-marker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :settings="{
+          coordinates: marker.coordinates,
+          onClick: () => (openMarker = index),
+          zIndex: openMarker === index ? 1 : 0,
+        }"
+      >
+        <div class="marker">
+          <div v-if="openMarker === index" class="popup">
+            <a href="/gallery" class="image-container"
+              ><img class="map-popup-img" :src="marker.properties.src" /><span
+                class="hover-text"
+                >В галерею</span
+              ></a
+            >
+            <b>{{ marker.properties.address }}</b>
+            <router-link class="map-popup-title" to="/gallery">{{
+              marker.properties.title
+            }}</router-link>
+          </div>
+        </div>
+      </yandex-map-marker>
     </yandex-map>
     <q-img src="Images/Gallery/1.png"> </q-img>
 
@@ -106,16 +141,60 @@ const markers = [
   color: #fff;
   font-weight: bold;
   line-height: 20px;
+  cursor: pointer;
 }
-.hint-window {
+.popup {
   position: absolute;
+  top: calc(-900%);
+  left: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
   transform: translate(15px, -10%);
-  padding: 4px 10px;
-  background: var(--q-primary);
-  color: var(--color-white);
-  border: 1px solid var(--q-primary);
+  padding: 8px;
+  background: var(--color-white);
+  color: var(--color-black);
+  -webkit-box-shadow: 0px 0px 15px 3px rgba(9, 9, 9, 0.2);
+  -moz-box-shadow: 0px 0px 15px 3px rgba(9, 9, 9, 0.2);
+  box-shadow: 0px 0px 15px 3px rgba(9, 9, 9, 0.2);
   white-space: nowrap;
   border-radius: 4px;
   font-size: 12px;
+}
+.map-popup-img {
+  width: 100%;
+  min-width: 140px;
+  border-radius: 2px;
+}
+.map-popup-title {
+  color: var(--color-black);
+  transition: 0.3s linear;
+}
+.image-container {
+  position: relative;
+  display: inline-block;
+}
+
+.hover-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+@media (hover: hover) {
+  .map-popup-title:hover {
+    color: var(--q-primary);
+  }
+  .image-container:hover .hover-text {
+    opacity: 1;
+  }
 }
 </style>
